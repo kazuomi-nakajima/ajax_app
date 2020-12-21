9e6f358d6be4548d72a994567e90da02882bf0ad
kazuomi-nakajima <x15076.nakajima@gmail.com>
2020-12-21 12:40:03 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,10 @@
 class PostsController < ApplicationController
   def index
-    # @post = Post.find(1)
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-    
-  end
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
